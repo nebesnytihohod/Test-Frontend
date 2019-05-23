b59702b2a10d6227a02f04f16cad74c4f42dfc5f
Maxim Stasenkov <makcct@yandex.ru>
2019-05-23 16:17:39 +0300

    REFACTOR (reader.clj): correction of comments for VERITA-121 PR #210
    1. Refactor-rename atom *res --> ^:dynamic *res*
    2. Delete function print-res
    3. Refactor-rename symbols i, k, l --> ts-level, tc-level, test-level

--- src/test_frontend/reader.clj
+++ src/test_frontend/reader.clj
@@ -17,26 +17,22 @@
       (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
 
 ;; vector of maps containing result of testing
-(def *res (atom []))
+(def ^:dynamic *res* (atom []))
 
 ;; mutable variable for storage number of 'false' test results (i.e. test fail)
 (def summary (ref 0))
-
-(defn print-res
-  []
-  (identity @*res))
 
 ;; deprecated function
 (defn log!
   "Function for execute and simple logging of result into atom structure"
   [text tc-id tc-n t-s descr v-expect f driver q opt]
-  (swap! *res conj {:test-case-id   tc-id
-                    :test-case-name tc-n
-                    :test-step      t-s
-                    :description    descr
-                    :text           text
-                    :expect         v-expect
-                    :actual         (f driver q opt)})
+  (swap! *res* conj {:test-case-id  tc-id
+                     :test-case-name tc-n
+                     :test-step      t-s
+                     :description    descr
+                     :text           text
+                     :expect         v-expect
+                     :actual         (f driver q opt)})
   driver)
 
 (def pm-dir
@@ -64,15 +60,15 @@
    (let [actual (with-postmortem driver pm-opt (if opt
                                                  (f driver q opt)
                                                  (f driver q)))]
-     (swap! *res conj {:test-case-id   tc-id
-                       :test-case-name tc-n
-                       :test-step      t-s
-                       :description    descr
-                       :text           text
-                       :expect         v-expect
-                       :actual         actual
-                       :conclusion     (identical? v-expect (if (map? actual) (:value actual) actual))})
-     (if (false? (:conclusion (last @*res))) (dosync (ref-set summary (inc @summary)))))
+     (swap! *res* conj {:test-case-id  tc-id
+                        :test-case-name tc-n
+                        :test-step      t-s
+                        :description    descr
+                        :text           text
+                        :expect         v-expect
+                        :actual         actual
+                        :conclusion     (identical? v-expect (if (map? actual) (:value actual) actual))})
+     (if (false? (:conclusion (last @*res*))) (dosync (ref-set summary (inc @summary)))))
    driver))
 
 (defn test-launch
@@ -98,38 +94,37 @@
   [test-edn-path]
   (try
     (let [test-edn (load-edn test-edn-path)]
-      (doseq [i test-edn]
-        (let [driver ((resolve (:test-suite-browser i)))]
-          (when (:test-suite-url i)
+      (doseq [ts-level test-edn]
+        (let [driver ((resolve (:test-suite-browser ts-level)))]
+          (when (:test-suite-url ts-level)
             (log-with-pm! "test preparation"
-                          (:test-suite-id i)
-                          (:test-suite-name i)
+                          (:test-suite-id ts-level)
+                          (:test-suite-name ts-level)
                           "step 0"
-                          (:test-suite-description i)
+                          (:test-suite-description ts-level)
                           nil
                           go
                           driver
-                          (:test-suite-url i)))
-          (doseq [k (:test-suite-set i)
-                  l (:test-case-set k)]
-            (log-with-pm! (:test-step-text l)
-                          (:test-case-id k)
-                          (:test-case-name k)
-                          (:test-step-id l)
-                          (:test-case-description k)
-                          (:expect l)
-                          (resolve (:test-function l))
+                          (:test-suite-url ts-level)))
+          (doseq [tc-level (:test-suite-set ts-level)
+                  test-level (:test-case-set tc-level)]
+            (log-with-pm! (:test-step-text test-level)
+                          (:test-case-id tc-level)
+                          (:test-case-name tc-level)
+                          (:test-step-id test-level)
+                          (:test-case-description tc-level)
+                          (:expect test-level)
+                          (resolve (:test-function test-level))
                           driver
-                          (:el-query l)
-                          (:func-opt l)))
+                          (:el-query test-level)
+                          (:func-opt test-level)))
           (quit driver)))                                  ; TODO: add a condition to close the browser window/session -> if (i :close-session == true) then (quit driver)
       @summary)
     (catch Exception e
       (spit pm-file (.getMessage e))
       (throw e))
     (finally
-      (spit result-file (print-res)))))
+      (spit result-file (identity @*res*)))))
 
 (comment
-  (test-launch "src/test_frontend/input_test.edn")
-  (print-res))
+  (test-launch "src/test_frontend/input_test.edn"))
